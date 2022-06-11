23b1a95c60005f5b1dad5c078ead38286533131a
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-11 15:22:46 +0300

    commit tree d38ec797df0cfa0753403228d253211a1f38e5aa

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(element) {
+        return element.startsWith(string);
+    });
+}
+
+function matchName(array, name) {
+    return array.filter(function(driver) {
+        return driver.name === name;
+    });
+}
